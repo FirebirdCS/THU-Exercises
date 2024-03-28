6335e4732ffe8bf3e8d842d0f7b2083377ce003a
FirebirdCS <alvaritotutos@gmail.com>
2024-03-28 10:33:23 -0600

    Steps 6 to 7 done, toDo done

--- Second module/src/classes/Project.ts
+++ Second module/src/classes/Project.ts
@@ -2,6 +2,8 @@ import {v4 as uuidv4} from 'uuid'
 
 export type projectStatus = "pending" | "active" | "finished"
 export type userRole = "architect" | "engineer" | "developer"
+export type statusTask = "important" | "completed" | "on-going" 
+export type statusColors = "red" | "green" | "orange"
 
 export interface IProject {
   name: string
@@ -9,6 +11,41 @@ export interface IProject {
   status: "pending" | "active" | "finished"
   role: "architect" | "engineer" | "developer"
   date: Date
+  todoList: toDo[]
+}
+
+
+export interface ITodo{
+  description: string
+  date: Date
+}
+
+export class toDo implements ITodo{
+  description: string
+  date: Date
+  uiTodo: HTMLElement
+  constructor(data: ITodo){
+    for (const key in data) {
+      this[key] = data[key]
+    }
+    this.setUI()
+  }
+  setUI(){
+    if(this.uiTodo && this.uiTodo instanceof HTMLElement) {return}
+    this.uiTodo = document.createElement("div")
+    this.uiTodo.className = "task-item"
+    // Way to format the date in a correct way without showing an offset day, like in project details
+    const localDate = new Date(this.date.getTime() + this.date.getTimezoneOffset() * 60000);
+    const formattedDate = localDate.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: '2-digit' });
+    this.uiTodo.innerHTML = `<div style="display: flex; justify-content: space-between; align-items: center;">
+      <div style="display: flex; column-gap: 15px; align-items: center;">
+        <span class="material-icons-round" style="padding: 10px; background-color: #686868; border-radius: 10px;">construction</span>
+        <p>${this.description}</p>
+      </div>
+      <p style="text-wrap: nowrap; margin-left: 10px;">${formattedDate}</p>
+    </div>`;
+}
+
 }
 
 
@@ -23,6 +60,7 @@ export class Project implements IProject {
   cost: number = 0
   progress: number = 0.6
   cardColor: string;
+  todoList: toDo[] = []
 
 
   constructor(data: IProject) {
@@ -33,6 +71,12 @@ export class Project implements IProject {
     this.cardColor = this.selectRandomColor();
     this.setUI()
   }
+
+  newTodo(data:ITodo){
+    const toDoList = new toDo(data)
+    this.todoList.push(toDoList)
+    return toDoList
+}
 
   setUI(){
         // Project card UI
@@ -73,4 +117,5 @@ export class Project implements IProject {
     const colors = ["#212B37", "#EF6337", "#781239", "#3b95bf", "#48bf3b"];
     return colors[Math.floor(Math.random() * colors.length)];
   }
+  
 }
