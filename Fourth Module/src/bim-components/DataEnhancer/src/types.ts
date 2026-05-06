8013cd141cab1775e992f37bc2ccf0481d4b030c
FirebirdCS <alvaritotutos@gmail.com>
2026-05-06 17:58:48 -0600

    Lesson 4.2 done, added setup for our custom component

--- Fourth Module/src/bim-components/DataEnhancer/src/types.ts
+++ Fourth Module/src/bim-components/DataEnhancer/src/types.ts
@@ -1,3 +1,5 @@
+import * as FRAGS from "@thatopen/fragments"
 export interface DataEnhancerSource {
     data: () => Promise<any[]>;
+    matcher: (attrs: FRAGS.ItemData, data: any[]) => any[] | null
 }
