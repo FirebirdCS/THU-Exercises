44f06c2786a8d5f164a6a803ac511b164409c72d
FirebirdCS <alvaritotutos@gmail.com>
2026-05-19 08:55:39 -0600

    Changed highlighter color

--- Fourth Module/src/bim-components/setup/src/highlighter.ts
+++ Fourth Module/src/bim-components/setup/src/highlighter.ts
@@ -1,7 +1,11 @@
+import * as THREE from "three"
 import * as OBC from "@thatopen/components"
 import * as OBF from "@thatopen/components-front"
 
 export const setupHighlighter = (components: OBC.Components, world: OBC.World) => {
     const highlighter = components.get(OBF.Highlighter)
-    highlighter.setup({world})
+    highlighter.setup({
+        world,
+        selectionColor: new THREE.Color("#feca29"),
+    })
 }
